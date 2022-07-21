cfb7593311ad5bc795e36336f9b47909b594e69b
SFXDevel <sfxdev.py@gmail.com>
2022-07-21 15:53:30 -0400

    complete filetree rework

--- run.sh
+++ run.sh
@@ -1,24 +1,28 @@
+#generate binary file tree
+mkdir __temp__
+mkdir __temp__/binaries
+
 #assemble boot.s file
-as --32 boot.s -o boot.o
+as --32 boot/scripts/boot.s -o __temp__/binaries/boot.o
 
 #compile kernel.c file
-gcc -m32 -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
+gcc -m32 -c kernel/kernel.c -o __temp__/binaries/kernel.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
 
-gcc -m32 -c utils.c -o utils.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
+gcc -m32 -c kernel/utils.c -o __temp__/binaries/utils.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
 
-gcc -m32 -c char.c -o char.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
+gcc -m32 -c kernel/char.c -o __temp__/binaries/char.o -std=gnu99 -ffreestanding -O1 -Wall -Wextra
 
 #linking the kernel with kernel.o and boot.o files
-ld -m elf_i386 -T linker.ld kernel.o utils.o char.o boot.o -o MyOS.bin -nostdlib
+ld -m elf_i386 -T linker.ld __temp__/binaries/kernel.o __temp__/binaries/utils.o __temp__/binaries/char.o __temp__/binaries/boot.o -o grub/boot/MyOS.bin -nostdlib
 
 #check MyOS.bin file is x86 multiboot file or not
-grub-file --is-x86-multiboot MyOS.bin
+grub-file --is-x86-multiboot grub/boot/MyOS.bin
 
 #building the iso file
-mkdir -p isodir/boot/grub
-cp MyOS.bin isodir/boot/MyOS.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o MyOS.iso isodir
+grub-mkrescue -o MyOS.iso grub
+
+#cleaning up the mess :)
+rm -r __temp__
 
 #run it in qemu
 qemu-system-x86_64 -cdrom MyOS.iso
